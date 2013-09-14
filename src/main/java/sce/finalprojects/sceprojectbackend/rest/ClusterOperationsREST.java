267f095c2da2b227762ab1e6188f240f54fd0399
Boris L. <borislit@gmail.com>
2013-09-14 15:08:24 +0200

    Remove the wrapper response object
    Done to ease the integration with client

--- src/main/java/sce/finalprojects/sceprojectbackend/rest/ClusterOperationsREST.java
+++ src/main/java/sce/finalprojects/sceprojectbackend/rest/ClusterOperationsREST.java
@@ -1,6 +1,8 @@
 package sce.finalprojects.sceprojectbackend.rest;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -11,7 +13,10 @@ import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Service;
 
+import com.sun.istack.NotNull;
+
 import sce.finalprojects.sceprojectbackend.database.DatabaseOperations;
+import sce.finalprojects.sceprojectbackend.datatypes.ClusterRepresentationDO;
 import sce.finalprojects.sceprojectbackend.datatypes.GetClustersResponse;
 import sce.finalprojects.sceprojectbackend.utils.MarkupUtility;
 
@@ -53,10 +58,21 @@ public class ClusterOperationsREST {
 				and the ID's of its sub-clusters
 	 */
 	@GET
-	public Response getClusters(@QueryParam("articleid") String articleID, @QueryParam("level") int level,  @QueryParam("clusterid") List<String> requestedClusters){
-		GetClustersResponse response = new GetClustersResponse();
-		response.setClusterRepresentation(DatabaseOperations.getClustersRepresentationByIDs(requestedClusters, level, articleID));
-		return Response.ok(response).build();
+	public Response getClusters(@NotNull @QueryParam("articleid") String articleID, @NotNull @QueryParam("level") int level,  @QueryParam("clusterid") List<String> requestedClusters){
+
+		Set<ClusterRepresentationDO> clusterRepresentation = new HashSet<ClusterRepresentationDO>();
+		if(level == 0){
+			//TODO
+		}else{
+			//TODO STUB - REMOVE - START
+			Set<String> children = new HashSet<String>();
+			children.add("1234");
+			children.add("2223");
+			clusterRepresentation.add(new ClusterRepresentationDO("222", "Test", children));
+			//TODO STUB - REMOVE - END
+			//return Response.ok(DatabaseOperations.getClustersRepresentationByIDs(requestedClusters, level, articleID)).build();
+		}
+		return Response.ok(clusterRepresentation).build();
 	}
 
 }
